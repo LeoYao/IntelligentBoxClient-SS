560fb1d7f787d930386b74f67678daefe3ae874f
Leo Yao <leo.yao.dev@gmail.com>
2016-03-17 11:30:52 -0400

    init

---
+++ SychronizationService/src/main/java/IntelligentBoxClient/SychronizationService/App.java
@@ -0,0 +1,13 @@
+package IntelligentBoxClient.SychronizationService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
