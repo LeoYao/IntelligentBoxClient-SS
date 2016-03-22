f86781e2c812a2c5c064d6f33551325efdb35a05
Leo Yao <leo.yao.dev@gmail.com>
2016-03-22 17:12:34 -0400

    sample for listFolderContinue

--- src/main/java/intelligentBoxClient/ss/SychronizationService.java
+++ src/main/java/intelligentBoxClient/ss/SychronizationService.java
@@ -5,7 +5,6 @@ package intelligentBoxClient.ss;
  */
 
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Locale;
 
 import com.dropbox.core.DbxAppInfo;
@@ -42,29 +41,70 @@ public class SychronizationService {
         DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         FullAccount account = client.users().getCurrentAccount();
-        System.out.println(account.getName().getDisplayName());
+        System.out.println(account.getAccountId());
+
+        // Upload "test.txt" to Dropbox
+        try (InputStream in = new FileInputStream("test.txt")) {
+            FileMetadata metadata = client.files().uploadBuilder("/test1/test.txt")
+                    .withMode(WriteMode.OVERWRITE)
+                    .uploadAndFinish(in);
+        }
+
+        ListFolderGetLatestCursorResult cursorResult =
+                client.files().listFolderGetLatestCursorBuilder("").withRecursive(true).withIncludeDeleted(true).start();
+
+        String cursor = cursorResult.getCursor();
 
         // Get files and folder metadata from Dropbox root directory
-        for (int i = 0; i < 2; i++) {
-            List<Metadata> entries = client.files().listFolder("").getEntries();
-            for (Metadata metadata : entries) {
+        boolean first = true;
+        ListFolderResult listFolderResult = client.files().listFolder("");
+        while (first || listFolderResult.getHasMore()) {
+            first = false;
+            for (Metadata metadata : listFolderResult.getEntries()) {
                 if (metadata instanceof FolderMetadata) {
                     FolderMetadata folderMetadata = (FolderMetadata) metadata;
-
+                    System.out.print(folderMetadata.getPathDisplay());
                     //System.out.println("\t" + folderMetadata.());
                 } else if (metadata instanceof FileMetadata) {
-                    System.out.print(metadata.getName());
                     FileMetadata fileMetadata = (FileMetadata) metadata;
-                    System.out.println("\t" + fileMetadata.getRev());
+
+                    System.out.print(fileMetadata.getPathDisplay());
+                    System.out.print("\t" + fileMetadata.getRev());
+                } else if (metadata instanceof DeletedMetadata) {
+                    DeletedMetadata deletedMetadata = (DeletedMetadata) metadata;
+
+                    System.out.print(deletedMetadata.getPathDisplay());
+                    System.out.print("\tdeleted");
                 }
 
+                System.out.println();
             }
+        }
 
-            // Upload "test.txt" to Dropbox
-            try (InputStream in = new FileInputStream("test.txt")) {
-                FileMetadata metadata = client.files().uploadBuilder("/test.txt")
-                        .withMode(WriteMode.OVERWRITE)
-                        .uploadAndFinish(in);
+        client.files().delete("/test1/test.txt");
+        System.out.println();
+        first = true;
+        listFolderResult = client.files().listFolderContinue(cursor);
+        while (first || listFolderResult.getHasMore()) {
+            for (Metadata metadata : listFolderResult.getEntries()) {
+                first = false;
+                if (metadata instanceof FolderMetadata) {
+                    FolderMetadata folderMetadata = (FolderMetadata) metadata;
+                    System.out.print(folderMetadata.getPathDisplay());
+                    //System.out.println("\t" + folderMetadata.());
+                } else if (metadata instanceof FileMetadata) {
+                    FileMetadata fileMetadata = (FileMetadata) metadata;
+
+                    System.out.print(fileMetadata.getPathDisplay());
+                    System.out.print("\t" + fileMetadata.getRev());
+                } else if (metadata instanceof DeletedMetadata) {
+                    DeletedMetadata deletedMetadata = (DeletedMetadata) metadata;
+
+                    System.out.print(deletedMetadata.getPathDisplay());
+                    System.out.print("\tdeleted");
+                }
+
+                System.out.println();
             }
         }
     }
